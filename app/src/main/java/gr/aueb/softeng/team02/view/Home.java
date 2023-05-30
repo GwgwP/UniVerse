723bd7b754c4f931b084d2747386edc72996619e
Lydiacwall <88028518+Lydiacwall@users.noreply.github.com>
2023-05-30 12:20:20 +0300

    changes in navigation_bottom

--- app/src/main/java/gr/aueb/softeng/team02/view/Home.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/Home.java
@@ -1,16 +1,33 @@
 package gr.aueb.softeng.team02.view;
 
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import gr.aueb.softeng.team02.R;
 
 public class Home extends AppCompatActivity {
 
+
+    BottomNavigationView bottomNav;
+
+
+    HomeFragment homeFrag = new HomeFragment();
+    ProgressFragment prograssFrag = new ProgressFragment();
+    SearchFragment searchFrag = new SearchFragment();
+    SubmissionFragment submissionFrag = new SubmissionFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNav= findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container , homeFrag).commit();
     }
+
 }
