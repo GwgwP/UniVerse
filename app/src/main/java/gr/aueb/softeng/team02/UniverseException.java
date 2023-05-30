3dd8e56329122bcf95effebbb7e12ba3c12f6e7d
Panos Triantafillidis <p3200199@aueb.gr>
2023-05-30 12:19:03 +0300

    update backend

--- app/src/main/java/gr/aueb/softeng/team02/UniverseException.java
+++ app/src/main/java/gr/aueb/softeng/team02/UniverseException.java
@@ -1,2 +1,17 @@
-package gr.aueb.softeng.team02;public class UniverseException {
+package gr.aueb.softeng.team02;
+public class UniverseException extends RuntimeException{
+
+    public UniverseException() { }
+
+    public UniverseException(String message) {
+        super(message);
+    }
+
+    public UniverseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UniverseException(Throwable cause) {
+        super(cause);
+    }
 }
--- app/src/main/java/gr/aueb/softeng/team02/dao/DAOFactory.java
+++ app/src/main/java/gr/aueb/softeng/team02/dao/DAOFactory.java
@@ -1,2 +1,42 @@
-package gr.aueb.softeng.team02.dao;public class DAOFactory {
+package gr.aueb.softeng.team02.dao;
+
+import gr.aueb.softeng.team02.UniverseException;
+
+public abstract class DAOFactory {
+    private static DAOFactory factory = null;
+
+    /**
+     * Επιστρέφει το εργοστάσιο για την παραγωγή των
+     * αντικειμένων DAO.
+     * @return To εργοστάσιο για την παραγωγή των DAO αντικειμένων
+     */
+    public static DAOFactory getFactory() {
+        if (factory == null) {
+            String className = null;
+
+            if (System.getProperty("daofactory") != null) {
+                className = System.getProperty("daofactory");
+            }
+
+            try {
+                factory = (DAOFactory) Class.forName(className).newInstance();
+            } catch (Exception e) {
+                throw new UniverseException(e);
+            }
+        }
+        return factory;
+    }
+
+    /**
+     * Επιστρέφει το αντικείμενο για τη διεπαφή {@link StudentDAO}.
+     * @return Το αντικείμενο DAO.
+     */
+    public abstract StudentDAO getStudentDAO();
+
+    /**
+     * Επιστρέφει το αντικείμενο για τη διεπαφή {@link ItemDAO}.
+     * @return Το αντικείμενο DAO.
+     */
+    public abstract SecretaryDAO getSecretaryDAO();
+
 }
