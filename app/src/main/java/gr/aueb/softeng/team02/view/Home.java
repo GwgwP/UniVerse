1c48a5a29d05360e7804e2aaf1cd9f15451d7a30
Lydiacwall <88028518+Lydiacwall@users.noreply.github.com>
2023-05-30 12:54:01 +0300

    no error

--- app/src/main/java/gr/aueb/softeng/team02/view/Home.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/Home.java
@@ -3,31 +3,25 @@ package gr.aueb.softeng.team02.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import gr.aueb.softeng.team02.R;
 
 public class Home extends AppCompatActivity {
 
-
-    BottomNavigationView bottomNav;
-
-
-    HomeFragment homeFrag = new HomeFragment();
-    ProgressFragment prograssFrag = new ProgressFragment();
-    SearchFragment searchFrag = new SearchFragment();
-    SubmissionFragment submissionFrag = new SubmissionFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bottomNav= findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container , homeFrag).commit();
+
     }
 
 }
