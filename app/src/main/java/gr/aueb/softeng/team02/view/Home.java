4d38c8fbf6a68f8637118c557e79b8dd898432de
Lydiacwall <88028518+Lydiacwall@users.noreply.github.com>
2023-05-30 14:53:30 +0300

    testing

--- app/src/main/java/gr/aueb/softeng/team02/view/Home.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/Home.java
@@ -19,7 +19,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.student_home);
 
 
     }
