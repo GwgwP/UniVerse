00e6a1cc8ca96f255294b3fe7e0bb68969b13816
Georgia Petsa <gwgwpet2001@gmail.com>
2023-06-04 13:15:55 +0300

    progress & detailed grafes finished

--- app/src/main/java/gr/aueb/softeng/team02/view/Progress/ProgressFragment.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/Progress/ProgressFragment.java
@@ -140,29 +140,29 @@ public class ProgressFragment extends Fragment implements ProgressView {
 
     public void showAveragePerSemester(HashMap<Integer, Double> av_grades) {
         if (!av_grades.containsKey(1))
-            sem_1_1.setText(sem_1_1.getText()+ " " + "-");
-        else sem_1_1.setText(sem_1_1.getText()+ " "+String.valueOf(av_grades.get(1)));
+            sem_1_1.setText("-");
+        else sem_1_1.setText(String.valueOf(av_grades.get(1)));
         if (!av_grades.containsKey(2))
-            sem_2_2.setText(sem_2_2.getText()+ " "+"-");
-        else sem_2_2.setText(sem_2_2.getText()+ " "+ String.valueOf(av_grades.get(2)));
+            sem_2_2.setText("-");
+        else sem_2_2.setText(String.valueOf(av_grades.get(2)));
         if (!av_grades.containsKey(3))
-            sem_3_3.setText(sem_3_3.getText()+ " "+"-");
-        else sem_3_3.setText(sem_3_3.getText()+ " "+String.valueOf(av_grades.get(3)));
+            sem_3_3.setText("-");
+        else sem_3_3.setText(String.valueOf(av_grades.get(3)));
         if (!av_grades.containsKey(4))
-            sem_4_4.setText(sem_4_4.getText()+ " "+"-");
-        else sem_4_4.setText(sem_4_4.getText()+ " "+ String.valueOf(av_grades.get(4)));
+            sem_4_4.setText("-");
+        else sem_4_4.setText(String.valueOf(av_grades.get(4)));
         if (!av_grades.containsKey(5))
-            sem_5_5.setText(sem_5_5.getText()+ " "+"-");
-        else sem_5_5.setText(sem_5_5.getText()+ " "+String.valueOf(av_grades.get(5)));
+            sem_5_5.setText("-");
+        else sem_5_5.setText(String.valueOf(av_grades.get(5)));
         if (!av_grades.containsKey(6))
-            sem_6_6.setText(sem_6_6.getText()+ " "+"-");
-        else sem_6_6.setText(sem_6_6.getText()+ " "+String.valueOf(av_grades.get(6)));
+            sem_6_6.setText("-");
+        else sem_6_6.setText(String.valueOf(av_grades.get(6)));
         if (!av_grades.containsKey(7))
-            sem_7_7.setText(sem_7_7.getText()+ " "+"-");
-        else sem_7_7.setText(sem_7_7.getText()+ " "+String.valueOf(av_grades.get(7)));
+            sem_7_7.setText("-");
+        else sem_7_7.setText(String.valueOf(av_grades.get(7)));
         if (!av_grades.containsKey(8))
-            sem_8_8.setText(sem_8_8.getText()+ " "+"-");
-        else sem_8_8.setText(sem_8_8.getText()+ " "+String.valueOf(av_grades.get(8)));
+            sem_8_8.setText("-");
+        else sem_8_8.setText(String.valueOf(av_grades.get(8)));
     }
 
 }
