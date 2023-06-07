94bd93bb45f815140a1c6a2720851fdada36178e
Georgia Petsa <gwgwpet2001@gmail.com>
2023-06-07 15:40:30 +0300

    progress in academic year, not fixed bug

--- app/src/main/java/gr/aueb/softeng/team02/view/AcademicYear/AcademicYearFragment.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/AcademicYear/AcademicYearFragment.java
@@ -1,36 +1,42 @@
 package gr.aueb.softeng.team02.view.AcademicYear;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import gr.aueb.softeng.team02.R;
+import gr.aueb.softeng.team02.dao.Initializer;
+import gr.aueb.softeng.team02.memorydao.MemoryInitializer;
 import gr.aueb.softeng.team02.view.Authentication.UserLoginActivity;
 
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class AcademicYearFragment extends Fragment implements AcademicYearFragmentView{
+public class AcademicYearFragment extends Fragment implements AcademicYearFragmentView {
 
     private ArrayList<String> yearList;
+    private ArrayAdapter<String> adapter;
+    Initializer init;
     private AcademicYearFragmentViewModel model;
     private Spinner spinner;
     private Spinner semester_spinner;
     private Button submitButton;
     private View myView;
-
-    ImageView ectsX;
+    Button addYearButton;
 
     EditText ects;
-
 
 
     @Override
@@ -41,19 +47,52 @@ public class AcademicYearFragment extends Fragment implements AcademicYearFragme
         myView = inflater.inflate(R.layout.fragment_academic_year, container, false);
         spinner = (Spinner) myView.findViewById(R.id.spinner2);
         semester_spinner = (Spinner) myView.findViewById(R.id.spinnerSemester);
-        ects = myView.findViewById(R.id.ectsTxt);
-        ectsX = myView.findViewById(R.id.ectsx);
-
         Bundle bundle = getArguments();
         // int student_id = bundle.getInt("STUDENT_ID", 0);
+        init = new MemoryInitializer();
+        ects = myView.findViewById(R.id.ectsTxt);
+
 
         model = new AcademicYearFragmentViewModel();
         model.getPresenter().setView(this);
         model.getPresenter().initLists();
 
+        addYearButton = requireView().findViewById(R.id.add_year_button);
+        addYearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Open a dialog or input field to get the new year from the user
+                // For example, using an AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setTitle("Add Year");
+
+                final EditText input = new EditText(requireContext());
+                builder.setView(input);
+
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newYear = input.getText().toString();
+                        model.getPresenter().addAcademicYear(newYear);
+                        //years.add(newYear);
+                        //adapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+                //model.getPresenter().onSeeAcademicYear();
+            }
+        });
+
+
         submitButton = myView.findViewById(R.id.submitBtnSecretary);
-
-
 
         return myView;
     }
@@ -65,14 +104,11 @@ public class AcademicYearFragment extends Fragment implements AcademicYearFragme
     }
 
 
-
-    public String getECTS(){return ects.getText().toString().trim();}
-
-    @Override
-    public void initECTSX(String txt) {
-        ectsX.setVisibility(View.VISIBLE);
-        Toast.makeText(getContext(), txt, Toast.LENGTH_SHORT).show();
+    public String getECTS() {
+        return ects.getText().toString().trim();
     }
+
+
 
     @Override
     public void setForm(HashMap<String, Integer> subjects) {
@@ -84,13 +120,22 @@ public class AcademicYearFragment extends Fragment implements AcademicYearFragme
 
     }
 
-    @Override
-    public void showPassedMsg(String txt) {
 
-    }
 
     @Override
     public void startSubmission() {
+        int flag =0 ;
+        if (getSelectedYear() != null && getSelectedSemester()!=null && getECTS()!= null & Integer.parseInt(getECTS()) >= 30 && Integer.parseInt(getECTS())<=80)
+        {
+            flag =model.getPresenter().submitNewAcademicYear(getSelectedYear(), Integer.parseInt(getSelectedSemester()), Integer.parseInt(getECTS()));
+
+
+        }
+        if (flag == -1)
+        {
+            //TODO error in submision
+        }
+
 
     }
 
@@ -103,28 +148,27 @@ public class AcademicYearFragment extends Fragment implements AcademicYearFragme
 
         // Set the ArrayAdapter on the Spinner
         spinner.setAdapter(adapter);
+
     }
 
-    public void createSemesterList(ArrayList<String> semesters)
-    {
+    public void createSemesterList(ArrayList<String> semesters) {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, semesters);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         semester_spinner.setAdapter(adapter);
     }
-    public String getSelectedSemester(ArrayList<String> semesters)
-    {
-        return semesters.get(((Spinner) myView.findViewById(R.id.spinnerSemester)).getSelectedItemPosition());
-    }
+
     @Override
-    public String getSelectedYear(ArrayList<String> years) {
-        return years.get(((Spinner) myView.findViewById(R.id.spinner)).getSelectedItemPosition());
+    public String getSelectedSemester(){//ArrayList<String> semesters) {
+        int i = semester_spinner.getSelectedItemPosition();
+        return model.getPresenter().get_semesters().get(i);
+        //return semesters.get(((Spinner) myView.findViewById(R.id.spinnerSemester)).getSelectedItemPosition());
     }
 
-    public void showAlertMessage(String title, String txt) {
-        new AlertDialog.Builder(requireContext())
-                .setCancelable(true)
-                .setTitle(title)
-                .setMessage(txt)
-                .setPositiveButton(R.string.ok, null).create().show();
+    @Override
+    public String getSelectedYear(){ //ArrayList<String> years) {
+        int i = spinner.getSelectedItemPosition();
+        return model.getPresenter().get_semesters().get(i);
+        //return years.get(((Spinner) myView.findViewById(R.id.spinner)).getSelectedItemPosition());
     }
+
 }
