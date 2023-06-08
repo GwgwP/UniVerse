b1c9f0716dacc46003e4e864d42b4ac6fd05ce4c
Georgia Petsa <gwgwpet2001@gmail.com>
2023-06-08 16:10:54 +0300

    finished ac year management and registration

--- app/src/main/java/gr/aueb/softeng/team02/view/AcademicYear/AcademicYearForm/AcademicYearFragmentViewModel.java
+++ app/src/main/java/gr/aueb/softeng/team02/view/AcademicYear/AcademicYearForm/AcademicYearFragmentViewModel.java
@@ -1,14 +1,8 @@
-package gr.aueb.softeng.team02.view.AcademicYear;
+package gr.aueb.softeng.team02.view.AcademicYear.AcademicYearForm;
 
 import androidx.lifecycle.ViewModel;
 
-import gr.aueb.softeng.team02.dao.AcademicYearDAO;
 import gr.aueb.softeng.team02.memorydao.AcademicYearDAOMemory;
-import gr.aueb.softeng.team02.memorydao.GradeDAOMemory;
-import gr.aueb.softeng.team02.memorydao.OfferedSubjectDAOMemory;
-import gr.aueb.softeng.team02.memorydao.StudentDAOMemory;
-import gr.aueb.softeng.team02.memorydao.SubmissionDAOMemory;
-import gr.aueb.softeng.team02.view.Submission.SubmissionFragmentPresenter;
 
 public class AcademicYearFragmentViewModel extends ViewModel {
 
