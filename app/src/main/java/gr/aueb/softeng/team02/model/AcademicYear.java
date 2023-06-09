c69191b1a7c660792e32718d66402988a874eee3
Panos Triantafillidis <p3200199@aueb.gr>
2023-06-09 13:13:47 +0300

    set packages of test

--- app/src/main/java/gr/aueb/softeng/team02/model/AcademicYear.java
+++ app/src/main/java/gr/aueb/softeng/team02/model/AcademicYear.java
@@ -11,9 +11,20 @@ public class AcademicYear {
     private LocalDate gradeDateEven;
     private LocalDate gradeDateOdd;
 
+    /**
+     * Default constructor
+     */
     public AcademicYear() {
         this.circList = new HashMap<>();
     }
+
+    /**
+     * Constructor with parameters : ac_year, gradeDateEven, gradeDateOdd
+     *
+     * @param ac_year       the time stamp of the academic year
+     * @param gradeDateEven the day of the grade upload for the spring semester
+     * @param gradeDateOdd  the day of the grade upload for the winter semeter
+     */
     public AcademicYear(String ac_year, LocalDate gradeDateEven, LocalDate gradeDateOdd) {
         this.ac_year = ac_year;
         this.circList = new HashMap<>();//Integer, Circumscription>();
@@ -21,6 +32,12 @@ public class AcademicYear {
         this.gradeDateOdd = gradeDateOdd;
     }
 
+    /**
+     * A setter for the academic year time stamp
+     *
+     * @param ac_year the time stamp of the academic year
+     * @throws AcademicYearException in case it is not in the right format
+     */
     public void setAc_year(String ac_year) throws AcademicYearException {
         if (ac_year != null) {
             String regex = "\\d{4}-\\d{4}";
@@ -32,10 +49,21 @@ public class AcademicYear {
         throw new AcademicYearException("Invalid year");
     }
 
+    /**
+     * Get the time stamp of the academic year
+     *
+     * @return
+     */
     public String getAc_year() {
         return ac_year;
     }
 
+    /**
+     * Adds a circumscription if it not contains in the circumscription hash map for a specific semester
+     *
+     * @param c a given circumscription
+     * @throws AcademicYearException in case of : the new circumscription is not valid
+     */
     public void addCircumscription(Circumscription c) throws AcademicYearException {
         if (c != null) {
             if (this.circList.get(c.getSemester()) != null)
@@ -48,6 +76,13 @@ public class AcademicYear {
             throw new AcademicYearException("Invalid circumscription");
     }
 
+    /**
+     * Get the circumscription of thw academic year based on a semester
+     *
+     * @param semester the semester as a string
+     * @return a circumscription object or null
+     * @throws AcademicYearException if the circumscription doesn't exist in the academic year
+     */
     public Circumscription getCircumscription(int semester) throws AcademicYearException {
         if (this.circList.get(semester) != null)
             return this.circList.get(semester);
@@ -59,6 +94,12 @@ public class AcademicYear {
         return this.getCircumscription(semester).getEcts();
     }
 
+    /**
+     * Compares two objects based in the time stamp of the academic year
+     *
+     * @param other the other object
+     * @return true or false
+     */
     @Override
     public boolean equals(Object other) {
         if (other == null)
@@ -73,15 +114,30 @@ public class AcademicYear {
         return (this.ac_year.equals(year.ac_year));
     }
 
+    /**
+     * A hash code based on the time stamp
+     *
+     * @return an integer
+     */
     @Override
     public int hashCode() {
         return this.ac_year.hashCode();
     }
 
+    /**
+     * Get the grade upload day for the spring semester
+     *
+     * @return a local date object
+     */
     public LocalDate getGradeDateEven() {
         return gradeDateEven;
     }
 
+    /**
+     * Get the grade upload day for the winter semester
+     *
+     * @return a local date object
+     */
     public LocalDate getGradeDateOdd() {
         return gradeDateOdd;
     }
--- app/src/main/java/gr/aueb/softeng/team02/model/Circumscription.java
+++ app/src/main/java/gr/aueb/softeng/team02/model/Circumscription.java
@@ -9,6 +9,14 @@ public class Circumscription {
     private LocalDate start;
     private LocalDate end;
 
+    /**
+     * Constructor of Circumscription
+     *
+     * @param semester the semester of the circumscription as an integer
+     * @param ects     the ects of the circumscription as an integer
+     * @param start    the start date of the submission upload day
+     * @param end      the end date of the submission upload day
+     */
     public Circumscription(int semester, int ects, LocalDate start, LocalDate end) {
         this.ects = ects;
         this.semester = semester;
@@ -16,15 +24,30 @@ public class Circumscription {
         this.end = end;
     }
 
+    /**
+     * Set the ects of the circumscription
+     *
+     * @param ects the ects as an integer
+     */
     public void setEcts(int ects) {
         this.ects = ects;
     }
 
+    /**
+     * Get the ects of the circumscription
+     *
+     * @return the ects as an integer
+     */
     public int getEcts() {
         return this.ects;
     }
 
-    public void setSemester(int semester) throws Exception {
+    /**
+     * Get the semester of the circumscription
+     *
+     * @param semester the semester as an integer
+     */
+    public void setSemester(int semester) {
         this.semester = semester;
     }
 
@@ -44,10 +67,18 @@ public class Circumscription {
         return end;
     }
 
+    /**
+     *
+     * @param end
+     */
     public void setEnd(LocalDate end) {
         this.end = end;
     }
 
+    /**
+     * Check the validity of the circumscription, if the ects are between 30 & 130 and also the semester is a valid integer
+     * @return true or false
+     */
     public boolean checkValidity() {
         return this.ects >= 30 && this.ects <= 130 && this.semester >= 1 && this.semester <= 8;
     }
