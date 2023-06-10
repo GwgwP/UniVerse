e0ec795f5f505aca29260173edf65f210a5038bd
Georgia Petsa <gwgwpet2001@gmail.com>
2023-06-10 17:56:46 +0300

    progress form 70%

--- app/src/test/java/gr/aueb/softeng/team02/model/view/Progress/ProgressForm/ProgressFormPresenterTest.java
+++ app/src/test/java/gr/aueb/softeng/team02/model/view/Progress/ProgressForm/ProgressFormPresenterTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import gr.aueb.softeng.team02.dao.GradeDAO;
 import gr.aueb.softeng.team02.dao.Initializer;
 import gr.aueb.softeng.team02.memorydao.MemoryInitializer;
 import gr.aueb.softeng.team02.view.Progress.ProgressForm.ProgressPresenter;
@@ -12,6 +13,7 @@ public class ProgressFormPresenterTest {
     private Initializer init;
     private ProgressPresenter presenter;
     private ProgressFormViewStub view;
+    private GradeDAO grades;
 
     @Before
     public void setUp(){
@@ -20,19 +22,49 @@ public class ProgressFormPresenterTest {
         presenter = new ProgressPresenter();
         presenter.setView(view);
         presenter.setGradesDao(init.getGradeDAO());
+        grades = init.getGradeDAO();
+
         view.setStudent_id(3200125);
+
+
+    }
+    @Test
+    public void Testinitentodetailedgrades()
+    {
+        presenter.onSeeGrades();
+        Assert.assertEquals(1, view.getOnseegrades());
     }
     @Test
     public void getters_and_setters_test()
     {
+        view.setStudent_id(3200125);
+
         Assert.assertEquals(3200125, view.getStudent_id());
         Assert.assertNotEquals(3200155, view.getStudent_id());
         Assert.assertNotEquals(3200199, view.getStudent_id());
 
 
         presenter.getAverage(view.getStudent_id());
-        //view.setAverage(presenter.);
-       // Assert.assertEquals(8.7, view.getAverage(), 0.00005);
+        Assert.assertEquals(1, view.getShown_avg());
+        Assert.assertEquals(0, view.getOnseegrades());
+        Assert.assertEquals(0, view.getTimes_shown_avgs());
+        Assert.assertEquals(0, view.getNumber_of_passed_subjects());
+        Assert.assertEquals(0, view.getShown_passed_subjs());
+
+        presenter.getAGperSem(view.getStudent_id());
+        Assert.assertEquals(1, view.getShown_avg());
+        Assert.assertEquals(0, view.getOnseegrades());
+        Assert.assertEquals(1, view.getTimes_shown_avgs());
+        Assert.assertEquals(0, view.getNumber_of_passed_subjects());
+        Assert.assertEquals(0, view.getShown_passed_subjs());
+        Assert.assertEquals(0, view.getEctsShown());
+
+        presenter.getECTS(view.getStudent_id());
+        Assert.assertEquals(1, view.getEctsShown());
+
+        presenter.getNumOfSubs(view.getStudent_id());
+        Assert.assertEquals(3200125, view.getStudent_id());
+        Assert.assertEquals(1, view.getShown_passed_subjs());
 
     }
 }
