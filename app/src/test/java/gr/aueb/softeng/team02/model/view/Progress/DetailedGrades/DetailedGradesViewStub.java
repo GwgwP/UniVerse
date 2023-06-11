c9c944600e4e4c03c888ed9053206a56348dbc6a
Georgia Petsa <gwgwpet2001@gmail.com>
2023-06-12 01:41:54 +0300

    Updates  in Detailed Grades Test  with JavaDoc 100% coverage

--- app/src/test/java/gr/aueb/softeng/team02/model/view/Progress/DetailedGrades/DetailedGradesViewStub.java
+++ app/src/test/java/gr/aueb/softeng/team02/model/view/Progress/DetailedGrades/DetailedGradesViewStub.java
@@ -12,24 +12,6 @@ public class DetailedGradesViewStub implements DetailedGradesView {
     private int subjects_size;
     private int num_of_shown_subjs;
 
-    public int getSubjects_size() {
-        return subjects_size;
-    }
-
-
-
-    public int getNum_of_shown_subjs() {
-        return num_of_shown_subjs;
-    }
-
-
-
-    public int getReceived_avgs() {
-        return received_avgs;
-    }
-
-
-
     private int received_avgs;
     private double sem1;
     private double sem2;
@@ -42,23 +24,20 @@ public class DetailedGradesViewStub implements DetailedGradesView {
     private HashMap<Integer, HashMap<String, Integer>> gradings;
 
 
+    public int getReceived_avgs() {
+        return received_avgs;
+    }
+
 
     @Override
     public void receiveAverages() {
         this.received_avgs++;
-        this.sem1 = getSem1();
-        this.sem2 = getSem2();
-        this.sem3 = getSem3();
-        this.sem4 = getSem4();
-        this.sem5 = getSem5();
-        this.sem6 = getSem6();
-        this.sem7 = getSem7();
-        this.sem8 = getSem8();
     }
 
 
     @Override
     public void viewSub(HashMap<Integer, HashMap<String, Integer>> subjects) {
+        this.gradings = subjects;
         this.subjects_size = subjects.size();
         this.num_of_shown_subjs++;
     }
@@ -74,71 +53,6 @@ public class DetailedGradesViewStub implements DetailedGradesView {
         return gradings;
     }
 
-    public void setGradings(HashMap<Integer, HashMap<String, Integer>> gradings) {
-        this.gradings = gradings;
-    }
 
-    public double getSem1() {
-        return sem1;
-    }
 
-    public void setSem1(double sem1) {
-        this.sem1 = sem1;
-    }
-
-    public double getSem2() {
-        return sem2;
-    }
-
-    public void setSem2(double sem2) {
-        this.sem2 = sem2;
-    }
-
-    public double getSem3() {
-        return sem3;
-    }
-
-    public void setSem3(double sem3) {
-        this.sem3 = sem3;
-    }
-
-    public double getSem4() {
-        return sem4;
-    }
-
-    public void setSem4(double sem4) {
-        this.sem4 = sem4;
-    }
-
-    public double getSem5() {
-        return sem5;
-    }
-
-    public void setSem5(double sem5) {
-        this.sem5 = sem5;
-    }
-
-    public double getSem6() {
-        return sem6;
-    }
-
-    public void setSem6(double sem6) {
-        this.sem6 = sem6;
-    }
-
-    public double getSem7() {
-        return sem7;
-    }
-
-    public void setSem7(double sem7) {
-        this.sem7 = sem7;
-    }
-
-    public double getSem8() {
-        return sem8;
-    }
-
-    public void setSem8(double sem8) {
-        this.sem8 = sem8;
-    }
 }
